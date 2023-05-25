7707da23c8437f2e4e4b9576d1e32f944bbb17d7
Florian Contant <florian.contant@ymail.com>
2023-05-25 16:37:33 +0200

    rails watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
+  resources :lists, only: [:new, :create, :show, :index] do
+    resources :bookmarks, only: :create
+  end
+  resources :bookmarks, only: [:destroy]
 end
